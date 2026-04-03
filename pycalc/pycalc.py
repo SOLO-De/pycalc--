d97e072f4183a85072c220354f5329b333fb04ef
Souleymane Dembele <22505411@me.uvsq.fr>
2026-04-03 11:56:12 +0200

    update ajout,multication et soustraction

--- pycalc/pycalc.py
+++ pycalc/pycalc.py
@@ -1,8 +1,36 @@
 def add(a, b):
-    return a+b
+    res = a
+    if b > 0:
+        while (b - 1) >= 0:
+            b -= 1
+            res += 1
+    elif b < 0:
+        while (b + 1) <= 0:
+            b += 1
+            res -= 1
+    return res
 def subtract(a, b):
-  return a-b
+    res = a
+    if b > 0:
+        while (b - 1) >= 0:
+            b -= 1
+            res -= 1
+    elif b < 0:
+        while (b + 1) <= 0:
+            b += 1
+            res += 1
+    return res
 def multiply(a, b):
-  return a*b
+    res = a
+    if b > 0:
+        while (b - 1) >= 0:
+            b -= 1
+            res += a
+    elif b < 0:
+        while (b + 1) <= 0:
+            b += 1
+            res += a
+            res=-res
+    return res
 def divide(a, b):
   return a//b
