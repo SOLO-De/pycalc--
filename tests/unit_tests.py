6c70934a776a014a38b7a531d65de553ef430ab9
Souleymane Dembele <22505411@me.uvsq.fr>
2026-04-03 11:30:29 +0200

    ajout des fonction de test d'addition et de multiplication

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,15 +3,51 @@ import pycalc.pycalc as calculator
 
 class PyCalcUnitTests(unittest.TestCase):
   def test_add(self):
-    left = [5]
-    right = [9]
-    expected = [14]
+    left = [5,7,365,0,0,0,-50,-12,-4,32,4,-16,7]
+    right = [9,0,-2,445,0,-54,100,0,-49,-32,-6,16,5]
+    expected = [14,7,363,445,0,-54,50,-12,53,0,-2,0,-2]
     for i in range(len(left)):
       l = left[i]
       r = right[i]
       e = expected[i]
       with self.subTest(l=l, r=r, e=e):
         got = calculator.add(l, r)
+        self.assertEqual(got, e)
+  
+  def test_mult(self):
+    left = [5,5,5,0,0,0,-5,-5,-5]
+    right = [9,0,-9,9,0,-9,9,0,-9]
+    expected = [45,0,-45,0,0,0,-45,0,-45]
+    for i in range(len(left)):
+      l = left[i]
+      r = right[i]
+      e = expected[i]
+      with self.subTest(l=l, r=r, e=e):
+        got = calculator.multiply(l, r)
+        self.assertEqual(got, e)
+
+  def test_sous(self):
+    left = [5]
+    right = [9]
+    expected = [-4]
+    for i in range(len(left)):
+      l = left[i]
+      r = right[i]
+      e = expected[i]
+      with self.subTest(l=l, r=r, e=e):
+        got = calculator.subtract(l, r)
+        self.assertEqual(got, e)
+
+  def test_divi(self):
+    left = [45]
+    right = [9]
+    expected = [5]
+    for i in range(len(left)):
+      l = left[i]
+      r = right[i]
+      e = expected[i]
+      with self.subTest(l=l, r=r, e=e):
+        got = calculator.devide(l, r)
         self.assertEqual(got, e)
 
 if __name__ == '__main__':
